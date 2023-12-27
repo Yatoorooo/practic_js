37379d046e8de17deff0abaf6105d4582f89dd03
Yatoorooo <Egvarakcin@gmail.com>
2023-12-27 23:32:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let firstNumber = +prompt('Send your number');
+let twoNumber = +prompt('Send your number');
+let threeNumber = +prompt('Send your number');
+
+let sum = (firstNumber + twoNumber + threeNumber) /3;
+
+alert(`Medium average = ${sum}`);
